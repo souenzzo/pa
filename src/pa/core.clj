9f57842a69be5b4ea542c7f417feb87e279aef8c
Enzzo Cavallo <souenzzo@gmail.com>
2021-09-26 00:30:00 -0300

    +join impl

--- src/pa/core.clj
+++ src/pa/core.clj
@@ -18,21 +18,48 @@
     io/resource
     slurp
     lps/parse-schema
-    (lu/inject-resolvers {:Document/selectString (fn [_ {:keys [^String cssQuery]} {::keys [^Document document]}]
-                                                   (string/join ""
-                                                     (for [^Element el (.select document cssQuery)
-                                                           txt (.textNodes el)]
-                                                       (str txt))))
-                          :Query/hello           (fn [_ _ _]
-                                                   "World")
-                          :Query/scraper         (fn [{::keys [^HttpClient http-client]} {:keys [url]} _]
-                                                   (let [req (.build (HttpRequest/newBuilder (URI/create url)))
-                                                         res (.send http-client
-                                                               req
-                                                               (HttpResponse$BodyHandlers/ofInputStream))
-                                                         body ^InputStream (.body res)]
-                                                     {::body     body
-                                                      ::document (Jsoup/parse body (str StandardCharsets/UTF_8) (str url))
-                                                      ::headers  (into {} (.map (.headers res)))
-                                                      ::status   (.statusCode res)}))})
+    (lu/inject-resolvers {:Element/selectString (fn [_ {:keys [^String of ^String attribute]} {::keys [^Element element]}]
+                                                  (if attribute
+                                                    (let [^Element el (first (.select element of))]
+                                                      (.attr el attribute))
+                                                    (string/join ""
+                                                      (for [^Element el (.select element of)
+                                                            txt (.textNodes el)]
+                                                        (str txt)))))
+                          :Element/scraper      (fn [{::keys [^HttpClient http-client]} {:keys [url selectParams]} {::keys [^Element element]}]
+                                                  (let [url (apply format url
+                                                              (for [^String of selectParams
+                                                                    :when of]
+                                                                (string/join ""
+                                                                  (for [^Element el (.select element of)
+                                                                        txt (.textNodes el)]
+                                                                    (str txt)))))
+
+                                                        req (.build (HttpRequest/newBuilder (URI/create url)))
+                                                        res (.send http-client
+                                                              req
+                                                              (HttpResponse$BodyHandlers/ofInputStream))
+                                                        body ^InputStream (.body res)]
+                                                    {:url      url
+                                                     ::body    body
+                                                     ::element (Jsoup/parse body (str StandardCharsets/UTF_8) (str url))
+                                                     ::headers (into {} (.map (.headers res)))
+                                                     ::status  (.statusCode res)}))
+                          :Element/selectList   (fn [_ {:keys [^String of]} {::keys [^Element element]
+                                                                             :as    input}]
+                                                  (for [^Element el (.select element of)]
+                                                    (assoc input ::element el)))
+                          :Query/hello          (fn [_ _ _]
+                                                  "World")
+                          :Query/scraper        (fn [{::keys [^HttpClient http-client]} {:keys [url]} _]
+                                                  (let [req (.build (HttpRequest/newBuilder (URI/create url)))
+                                                        res (.send http-client
+                                                              req
+                                                              (HttpResponse$BodyHandlers/ofInputStream))
+                                                        body ^InputStream (.body res)]
+                                                    {:url      url
+                                                     ::body    body
+                                                     ::element (Jsoup/parse body (str StandardCharsets/UTF_8) (str url))
+                                                     ::headers (into {} (.map (.headers res)))
+                                                     ::status  (.statusCode res)}))})
     ls/compile))
--- test/pa/core_test.clj
+++ test/pa/core_test.clj
@@ -2,13 +2,15 @@
   (:require [pa.core :as pa]
             [hiccup2.core :as h]
             [clojure.test :refer [deftest is]]
-            [com.wsscode.pathom.connect.graphql2 :as pcg]
             [clojure.pprint :as pprint]
+            [ring.core.protocols]
             [clojure.java.io :as io]
-            [com.walmartlabs.lacinia :as lacinia])
-  (:import (java.net.http HttpResponse HttpClient HttpHeaders)
+            [com.walmartlabs.lacinia :as lacinia]
+            [clojure.string :as string])
+  (:import (java.net.http HttpResponse HttpClient HttpHeaders HttpRequest)
            (java.util.function BiPredicate)
-           (java.nio.charset StandardCharsets)))
+           (java.nio.charset StandardCharsets)
+           (java.io ByteArrayOutputStream)))
 ;; https://docs.oracle.com/en/java/javase/17/docs/api/java.xml/javax/xml/xpath/package-summary.html
 
 (set! *warn-on-reflection* true)
@@ -47,38 +49,124 @@ query {
       }
 ")
 
+
 (defn mock-http-client
-  [{::keys []}]
+  [{::keys [*urls ring-handler]
+    :or    {*urls        (atom [])
+            ring-handler (fn [req]
+                           {:body   (->> [:html
+                                          [:head
+                                           [:title "Hello"]]
+                                          [:body
+                                           [:div "World"]]]
+                                      (h/html {:mode :html})
+                                      str)
+                            :status 200})}}]
   (proxy [HttpClient] []
-    (send [req res]
-      (reify HttpResponse
-        (body [this]
-          (-> [:html
-               [:head
-                [:title "Hello"]]
-               [:body
-                [:div "World"]]]
-            (->> (h/html {:mode :html}))
-            str
-            (.getBytes (str StandardCharsets/UTF_8))
-            io/input-stream))
-        (headers [this] (HttpHeaders/of {}
-                          (reify BiPredicate
-                            (test [this a b]
-                              true))))
-        (statusCode [this]
-          200)))))
+    (send [^HttpRequest req res]
+      (swap! *urls conj (str (.uri req)))
+      (let [uri (.uri req)
+            {:keys [status body headers]
+             :as   response} (ring-handler
+                               {:request-method (keyword (string/lower-case (.method req)))
+                                :server-name    (.getHost uri)
+                                :uri            (str "/" (.getPath uri))
+                                :server-port    (.getPort uri)
+                                :scheme         (keyword (.getScheme uri))
+                                :protocol       "HTTP/1.1"})
+            baos (ByteArrayOutputStream.)]
+        (ring.core.protocols/write-body-to-stream body response baos)
+        (reify HttpResponse
+          (body [this]
+            (io/input-stream (.toByteArray baos)))
+          (headers [this] (HttpHeaders/of (into {}
+                                            (map (fn [[k v]]
+                                                   (if (coll? v)
+                                                     [k (into-array v)]
+                                                     [k (into-array [v])])))
+                                            headers)
+                            (reify BiPredicate
+                              (test [this a b]
+                                true))))
+          (statusCode [this] status))))))
 
 (deftest with-lacinia
   (let [query "query {
                  g1: scraper(url: \"https://g1.globo.com\") {
-                   title: selectString(cssQuery: \"head > title\"),
-                   description: selectString(cssQuery: \"body > div\")
+                   title: selectString(of: \"head > title\"),
+                   description: selectString(of: \"body > div\")
                  }
                }"
         variables {}
         options {}
         context {::pa/http-client (mock-http-client {})}]
+    (is (-> (lacinia/execute pa/schema query variables context options)
+          (doto pprint/pprint)
+          (= {:data {:g1 {:title       "Hello"
+                          :description "\nWorld"}}})))))
+
+
+
+(deftest join!
+  (let [query "query {
+                 g1: scraper(url: \"https://g1.globo.com\") {
+                   title: selectString(of: \"head > title\"),
+                   description: selectString(of: \"body > div\")
+                   twitter: scraper(url: \"https://twitter.com/%s\", selectParams: [\"head > title\"]) {
+                     lastTweet: selectString(of: \"div > main > div\")
+                   }
+                 }
+               }"
+        variables {}
+        options {}
+        *urls (atom [])
+        context {::pa/http-client (mock-http-client {::*urls        *urls
+                                                     ::ring-handler (fn [{:keys [server-name]}]
+                                                                      (let [html (case server-name
+                                                                                   "g1.globo.com" [:html
+                                                                                                   [:head
+                                                                                                    [:title "Hello"]]
+                                                                                                   [:body
+                                                                                                    [:div "world"]]]
+
+                                                                                   "twitter.com" [:html
+                                                                                                  [:head
+                                                                                                   [:title "tt"]]
+                                                                                                  [:body
+                                                                                                   [:div
+                                                                                                    [:main
+                                                                                                     [:div "hello-from-tt"]]]]])]
+                                                                        {:body    (->> html
+                                                                                    (h/html {:mode :html})
+                                                                                    str)
+                                                                         :headers {}
+                                                                         :status  200}))})}]
+    (is (-> (lacinia/execute pa/schema query variables context options)
+          (doto pprint/pprint)
+          (= {:data
+              {:g1
+               {:title       "Hello",
+                :description "\nworld",
+                :twitter     {:lastTweet "\nhello-from-tt"}}}})))
+    (is (= ["https://g1.globo.com" "https://twitter.com/Hello"]
+          @*urls))))
+
+
+
+(deftest v1
+  (let [query "
+ {
+   hackernews: scraper(url: \"https://news.ycombinator.com/\") {
+     url
+     news: selectList(of:\"tr.athing\") {
+       title: selectString(of:\"a\")
+       link: selectString(of:\"td.title > a\", attribute:\"href\")
+     }
+   }
+ }"
+        variables {}
+        options {}
+        context {::pa/http-client (HttpClient/newHttpClient)}]
     (is (-> (lacinia/execute pa/schema query variables context options)
           (doto pprint/pprint)
           (= {:data {:g1 {:title       "Hello"
